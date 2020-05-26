f3764892da292a592e7a2a0b332fedccbc3cf808
robert <rdcolema7@gmail.com>
2020-05-25 23:05:53 -0400

    added server

--- client/src/worldMap.js
+++ client/src/worldMap.js
@@ -2,6 +2,7 @@ import React, { useState, Component } from 'react';
 import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
 import RangeSlider from 'react-bootstrap-range-slider';
 import ReactMapGL from 'react-map-gl';
+import axios from 'axios';
 
 
 const Slider = () => {
@@ -22,28 +23,77 @@ const Slider = () => {
 
 };
 
-export default function WorldMap() {
-  const [viewport, setViewport] = useState({
-    width: 1500,
-    height: 750,
-    latitude: 11.0,
-    longitude: 9.0,
-    zoom: 2,
-    minZoom: 2,
-  });
+// export default function WorldMap() {
+//   const [viewport, setViewport] = useState({
+//     width: 1500,
+//     height: 750,
+//     latitude: 11.0,
+//     longitude: 9.0,
+//     zoom: 2,
+//     minZoom: 2,
+//   });
 
-  return (
-  	<div id="map-container">
-	    <ReactMapGL
-	      {...viewport}
-	      onViewportChange={setViewport}
-	      width="90w"
-      	  height="80vh"
-      	  mapStyle="mapbox://styles/mapbox/outdoors-v11"
-	      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-	    /> 
-	    <Slider />
-	</div>
-  );
+//   return (
+//   	<div id="map-container">
+// 	    <ReactMapGL
+// 	      {...viewport}
+// 	      onViewportChange={setViewport}
+// 	      width="90w"
+//       	  height="80vh"
+//       	  mapStyle="mapbox://styles/mapbox/outdoors-v11"
+// 	      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+// 	    /> 
+// 	    <Slider />
+// 	</div>
+//   );
+// }
+
+
+class WorldMap extends Component {
+
+  state = { 
+    viewport :{
+      width: 1500,
+      height: 750,
+      latitude: 11.0,
+      longitude: 9.0,
+      zoom: 2,
+      minZoom: 2
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:${process.env.REACT_APP_SERVER_PORT}/events`)
+      .then(res => {
+        const events = res.data;
+        console.log(events);
+      })
+  }
+
+  handleViewportChange = viewport => {
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
+    })
+  }
+
+  render() {
+
+    const { viewport } = this.state;
+
+    return (
+      <div id="map-container">
+        <ReactMapGL
+          {...viewport}
+          onViewportChange={this.handleViewportChange}
+          width="90w"
+            height="80vh"
+            mapStyle="mapbox://styles/mapbox/outdoors-v11"
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        /> 
+        <Slider />
+      </div>
+    );
+  }
 }
 
+export default WorldMap;
---
+++ server/index.js
@@ -0,0 +1,37 @@
+const cors = require('cors');
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST_IP,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+});
+
+// Cross-Origin Resource Sharing
+app.use(cors());
+
+app.listen(process.env.REACT_APP_SERVER_PORT, () => {
+  console.log(`App server now listening on port ${process.env.REACT_APP_SERVER_PORT}`);
+});
+
+
+// List wines
+app.get('/events', (req, res) => {
+
+  pool.query(`
+    SELECT * FROM timeline_event LIMIT 10`, 
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.send(err);
+      } else {
+        return res.send(results);
+      }
+    });
+});
+
+
